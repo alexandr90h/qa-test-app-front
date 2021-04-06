615b5fcfe46060a0d56a7f8a3e2a789963defd55
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2021-04-06 22:16:44 +0300

    add sass-module and styles

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,25 +1,27 @@
 import graf from '../../img/graf.png';
 import cat from '../../img/cat.svg';
-import s from './Results.module.css';
+import s from './Results.module.scss';
 
 export default function Results() {
   return (
     <container className={s.mainConteiner}>
-      <h2 className={s.heading}>Results</h2>
-      <p>[ Testing theory_]</p>
-      <div></div>
+      <h2 className={s.title}>Results</h2>
+      <p className={s.textContent1}>[ Testing theory_]</p>
+      <div className={s.lineHorizontal}></div>
       <div>
-        <img src={graf} alt="" />
+        <img className={s.imgGraf} src={graf} alt="" />
       </div>
-      <div>
+      <div className={s.resultTextContent}>
         <p>Correct answers - 9</p>
-        <div></div>
+        <div className={s.lineVertical}></div>
         <p>Total questions - 12</p>
       </div>
-      <img src={cat} alt="" />
-      <p>Not bad!</p>
-      <p>But you still need to learn some materials.</p>
-      <button>Try again</button>
+      <img className={s.catImage} src={cat} alt="" />
+      <p className={s.grateText}>Not bad!</p>
+      <p className={s.sentenceText}>
+        But you still need to learn some materials.
+      </p>
+      <button className={s.btnTryAgain}>Try again</button>
     </container>
   );
 }
