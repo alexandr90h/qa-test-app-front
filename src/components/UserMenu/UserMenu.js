2db2969345c49e1913f0d014e646349e0228b29b
Liliia Novikova <kplily85@gmail.com>
2021-04-06 16:57:35 +0300

    opens user menu

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { NavLink } from 'react-router-dom';
 import s from '../UserMenu/UserMenu.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUserName } from '../../redux/authorization/authorization-selectors';
 import { logOut } from '../../redux/authorization/authorization-operations';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 export default function UserMenu() {
   const dispatch = useDispatch();
   const userName = useSelector(getUserName);
-  const nameInAvatar = userName[0].toUpperCase();
+  // const nameInAvatar = userName[0].toUpperCase();
+  const nameInAvatar = 'U';
 
   return (
     <div>
       <NavLink exact to="/home" className={s.link}>
         Home
       </NavLink>
-      <NavLink exact to="/materials" className={s.link}>
+      <NavLink exact to="/usefulinfo" className={s.link}>
         Materials
       </NavLink>
       <Avatar>{nameInAvatar}</Avatar>
