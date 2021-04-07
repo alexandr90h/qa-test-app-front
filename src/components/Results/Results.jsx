aa662c8a36886b0cb8f35ce54a174a8664d86a73
Olexandr <63474923+alexandr90h@users.noreply.github.com>
2021-04-07 16:32:41 +0300

    add responsive

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,20 +1,22 @@
-import graf from '../../img/graf.png';
 import cat from '../../img/cat.svg';
 import s from './Results.module.scss';
 
 export default function Results() {
+  const rightAnswers = 9;
   return (
     <container className={s.mainConteiner}>
       <h2 className={s.title}>Results</h2>
       <p className={s.textContent1}>[ Testing theory_]</p>
       <div className={s.lineHorizontal}></div>
-      <div>
-        <img className={s.imgGraf} src={graf} alt="" />
-      </div>
+      <div></div>
       <div className={s.resultTextContent}>
-        <p>Correct answers - 9</p>
+        <p>
+          Correct answers - <span>{rightAnswers}</span>
+        </p>
         <div className={s.lineVertical}></div>
-        <p>Total questions - 12</p>
+        <p>
+          Total questions - <span>{12 - rightAnswers}</span>
+        </p>
       </div>
       <img className={s.catImage} src={cat} alt="" />
       <p className={s.grateText}>Not bad!</p>
