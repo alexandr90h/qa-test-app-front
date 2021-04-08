00e59b645c7a0ada25463145dc13c9e6884237b2
unknown <63597446+antonlazurko@users.noreply.github.com>
2021-04-08 12:50:43 +0300

    implemeted sprite-svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,14 @@
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
-import Copyright from './images/Copyright.svg';
-import favoriteHeart from './images/Favorite-heart.svg';
-
+import sprite from '../../img/sprite.svg';
 const Footer = () => (
   <footer className={styles.footer}>
-    {/* <div>
-      <p className={styles.footerText}>
-        <span className={styles.copyright}>
-          <img src={Copyright} alt="copyright" />
-        </span>
-        2021 | All Rights Reserved | Developed with
-        <img src={favoriteHeart} alt="heart" />
-        <svg className="footerIcon" width="17" height="17">
-          <use href="./img/sprite.svg#heart"></use>
-        </svg>
-        by&nbsp;
-        <Link to="/contacts" className="footerContacts">
-          GoIT Students
-        </Link>
-      </p>
-    </div> */}
     <span className={styles.footerText}>
-      <img src={Copyright} alt="copyright" />
-      &nbsp; 2021 | All rights Reserved | Developed with&nbsp;
-      <img src={favoriteHeart} alt="heart" />
+      <span className={styles.copyright}> © </span>
+      2021 | All rights Reserved | Developed with&nbsp;
+      <svg className={styles.footerIcon} width="17" height="17">
+        <use href={sprite + '#heart'}></use>
+      </svg>
       &nbsp; by &nbsp;
       <Link to="/contacts" className={styles.footerContacts}>
         GoIT Students
